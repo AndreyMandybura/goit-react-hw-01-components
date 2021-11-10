a479ae98ff5bdcca43391ed9e07a8d78d19121c0
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2021-11-10 14:57:16 +0200

    Добавил компонент  TansactionHistory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import user from './user.json';
 import statisticalData from './statistical-data.json';
 import friends from './friends.json';
+import transactions from './transactions.json';
 import SocialProfile from './components/SocialProfile/SocialProfile';
 import Statistics from './components/StatisticalData/StatisticalData';
 import FriendList from './components/Friends/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 export default function App() {
   return (
@@ -15,10 +17,9 @@ export default function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={statisticalData} />
-
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 }
